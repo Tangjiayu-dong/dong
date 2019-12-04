f62589ffebccc9fbb479597f31e89551721ee886
unknown <Tangjiayu-dong>
2019-12-04 10:47:32 +0800

    this is a test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,122 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>消息</header>
+    <input type="text" placeholder="请输入关键字">
+    <div class="main" v-for="item in list" :key="item">
+      <img :src="item.img" alt />
+      <span>{{item.title}}</span>
+      <span>{{item.des}}</span>
+      <p>{{item.time}}</p>
+    </div>
+    <div class="footer">
+      <router-link to="/" tag="span">首页</router-link>
+      <router-link to="/about" tag="span">分类</router-link>
+      <router-link to="/my" tag="span">我的</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  data() {
+    return {
+      list: [
+        {
+          img: "https://f11.baidu.com/it/u=1243293019,1807664460&fm=72",
+          title: "我的同桌",
+          des: "作业写完了没 6666",
+          time: "17:00"
+        },
+        {
+          img: "https://f11.baidu.com/it/u=1243293019,1807664460&fm=72",
+          title: "我的同桌",
+          des: "作业写完了没 6666",
+          time: "17:00"
+        },
+        {
+          img: "https://f11.baidu.com/it/u=1243293019,1807664460&fm=72",
+          title: "我的同桌",
+          des: "作业写完了没 6666",
+          time: "17:00"
+        },
+        {
+          img: "https://f11.baidu.com/it/u=1243293019,1807664460&fm=72",
+          title: "我的同桌",
+          des: "作业写完了没 6666",
+          time: "17:00"
+        },
+        {
+          img: "https://f11.baidu.com/it/u=1243293019,1807664460&fm=72",
+          title: "我的同桌",
+          des: "作业写完了没 6666",
+          time: "17:00"
+        },
+        {
+          img: "https://f11.baidu.com/it/u=1243293019,1807664460&fm=72",
+          title: "我的同桌",
+          des: "作业写完了没 6666",
+          time: "17:00"
+        },
+        {
+          img: "https://f11.baidu.com/it/u=1243293019,1807664460&fm=72",
+          title: "我的同桌",
+          des: "作业写完了没 6666",
+          time: "17:00"
+        },
+        {
+          img: "https://f11.baidu.com/it/u=1243293019,1807664460&fm=72",
+          title: "我的同桌",
+          des: "作业写完了没 6666",
+          time: "17:00"
+        },
+        {
+          img: "https://f11.baidu.com/it/u=1243293019,1807664460&fm=72",
+          title: "我的同桌",
+          des: "作业写完了没 6666",
+          time: "17:00"
+        },
+        {
+          img: "https://f11.baidu.com/it/u=1243293019,1807664460&fm=72",
+          title: "我的同桌",
+          des: "作业写完了没 6666",
+          time: "17:00"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+header {
+  width: 100%;
+  height: 50px;
+  background: skyblue;
+  text-align: center;
+  line-height: 50px;
+  font-size: 30px;
+}
+.main {
+  flex: 1;
+  img{
+    width: 200px;
+    height: 200px;
   }
 }
-</script>
+.footer {
+  width: 100%;
+  height: 60px;
+  background: hotpink;
+  text-align: center;
+  line-height: 60px;
+  span {
+    padding: 50px;
+  }
+}
+</style>
